c9fae433b6660d741b287e84ac39040e6fa22a30
llNathanlc <llnathanlc@gmail.com>
2023-03-09 17:22:37 +0100

    improved code readability and added touch support

--- javascript.js
+++ javascript.js
@@ -8,6 +8,7 @@ const eraser = document.getElementById("eraser");
 const painterBlack = document.getElementById('black');
 const color = document.getElementById("colorChoose");
 const buttons = document.querySelectorAll('.buttons');
+const saveColor = document.getElementById('saveColor');
 
 let tileSize = 0;
 let down = false;
@@ -20,26 +21,27 @@ var lastX = 0,
     lastY = 0;
 let buttonPress = 'painting';
 
-function pushButtons(e){
-        e.addEventListener('mouseover', function () {
-            this.classList.add('mouseOver');
-        });
 
-        e.addEventListener('mouseout', function () {
-            this.classList.remove('mouseOver');
-        });
+function pushButtons(e) {
+    e.addEventListener('pointerover', function () {
+        this.classList.add('mouseOver');
+    });
 
-        e.addEventListener('mousedown', function () {
-            this.classList.add('clicked');
-        });
+    e.addEventListener('pointerout', function () {
+        this.classList.remove('mouseOver');
+    });
 
-        e.addEventListener('mouseup', function () {
-            this.classList.remove('clicked');
-        });
+    e.addEventListener('pointerdown', function () {
+        this.classList.add('clicked');
+    });
 
-        e.addEventListener('mouseleave', function () {
-            this.classList.remove('clicked');
-        });
+    e.addEventListener('pointerup', function () {
+        this.classList.remove('clicked');
+    });
+
+    e.addEventListener('pointerleave', function () {
+        this.classList.remove('clicked');
+    });
 }
 
 function paint(ex, ey) {
@@ -49,8 +51,6 @@ function paint(ex, ey) {
     if (buttonPress === 'erasing') {
         document.elementFromPoint(ex, ey).style.removeProperty('background-color');
     }
-
-
 }
 
 function createBoard(size) {
@@ -80,241 +80,69 @@ function createBoard(size) {
 function paintBlack() {
     paintingColor = 'black';
     buttonPress = 'painting';
-    board.addEventListener('mousedown', function (e) {
+    board.addEventListener('pointerdown', function (e) {
         document.elementFromPoint(e.pageX, e.pageY).style.backgroundColor = paintingColor;
         lastX = e.pageX;
         lastY = e.pageY;
         down = true;
     });
-    board.addEventListener('mouseup', function () {
+    board.addEventListener('pointerup', function () {
         down = false;
 
     });
 
-    board.addEventListener('mouseover', function (e) {
-        if (down) {
-            var mouseX = e.pageX;
-            var mouseY = e.pageY;
-
-            var x0 = mouseX,
-                y0 = mouseY,
-                x1 = lastX,
-                y1 = lastY;
-
-            var steep = (Math.abs(y1 - y0) > Math.abs(x1 - x0));
-            if (steep) {
-                var x = x0;
-                x0 = y0;
-                y0 = x;
-
-                var y = y1;
-                y1 = x1;
-                x1 = y;
-            }
-            if (x0 > x1) {
-                var x = x0;
-                x0 = x1;
-                x1 = x;
-
-                let y = y0;
-                y0 = y1;
-                y1 = y;
-            }
-            var dx = x1 - x0,
-                dy = Math.abs(y1 - y0),
-                error = 0,
-                de = dy / dx,
-                yStep = -1,
-                y = y0;
-        }
-        if (y0 < y1) {
-            yStep = 1;
-        }
-        for (var x = x0; x < x1; x++) {
-            if (steep) {
-                paint(y, x);
-            } else {
-                paint(x, y);
-            }
-
-            error += de;
-            if (error >= 0.5) {
-                y += yStep;
-                error -= 1.0;
-            }
-        }
-        lastX = mouseX;
-        lastY = mouseY;
+    board.addEventListener('pointermove', function (e) {
+        calculateCoordinates(e);
     });
-    window.addEventListener('mouseup', function () {
+    window.addEventListener('pointerup', function () {
         down = false;
     });
 }
+
 function paintColor() {
     paintingColor = color.value;
+    saveColor.style.backgroundColor = paintingColor;
     buttonPress = 'painting';
-    board.addEventListener('mousedown', function (e) {
+    board.addEventListener('pointerown', function (e) {
         document.elementFromPoint(e.pageX, e.pageY).style.backgroundColor = paintingColor;
         lastX = e.pageX;
         lastY = e.pageY;
         down = true;
     });
-    board.addEventListener('mouseup', function () {
+    board.addEventListener('pointerup', function () {
         down = false;
 
     });
 
-    board.addEventListener('mouseover', function (e) {
-        if (down) {
-            var mouseX = e.pageX;
-            var mouseY = e.pageY;
-
-            var x0 = mouseX,
-                y0 = mouseY,
-                x1 = lastX,
-                y1 = lastY;
-
-            var steep = (Math.abs(y1 - y0) > Math.abs(x1 - x0));
-            if (steep) {
-                var x = x0;
-                x0 = y0;
-                y0 = x;
-
-                let y = y1;
-                y1 = x1;
-                x1 = y;
-            }
-            if (x0 > x1) {
-                var x = x0;
-                x0 = x1;
-                x1 = x;
-
-                var y = y0;
-                y0 = y1;
-                y1 = y;
-            }
-            var dx = x1 - x0,
-                dy = Math.abs(y1 - y0),
-                error = 0,
-                de = dy / dx,
-                yStep = -1,
-                y = y0;
-        }
-        if (y0 < y1) {
-            yStep = 1;
-        }
-        for (var x = x0; x < x1; x++) {
-            if (steep) {
-                paint(y, x);
-            } else {
-                paint(x, y);
-            }
-
-            error += de;
-            if (error >= 0.5) {
-                y += yStep;
-                error -= 1.0;
-            }
-        }
-        lastX = mouseX;
-        lastY = mouseY;
+    board.addEventListener('pointermove', function (e) {
+        calculateCoordinates(e);
     });
-    window.addEventListener('mouseup', function () {
+    window.addEventListener('pointerup', function () {
         down = false;
     });
 }
-
 function eraseColor() {
     buttonPress = 'erasing';
-    board.addEventListener('mousedown', function (e) {
+    board.addEventListener('pointerdown', function (e) {
         document.elementFromPoint(e.pageX, e.pageY).style.removeProperty('background-color');
         lastX = e.pageX;
         lastY = e.pageY;
         down = true;
     });
-    board.addEventListener('mouseup', function () {
+    board.addEventListener('pointerup', function () {
         down = false;
 
     });
-
-    board.addEventListener('mouseover', function (e) {
-        if (down) {
-            var mouseX = e.pageX;
-            var mouseY = e.pageY;
-
-            var x0 = mouseX,
-                y0 = mouseY,
-                x1 = lastX,
-                y1 = lastY;
-
-            var steep = (Math.abs(y1 - y0) > Math.abs(x1 - x0));
-            if (steep) {
-                var x = x0;
-                x0 = y0;
-                y0 = x;
-
-                let y = y1;
-                y1 = x1;
-                x1 = y;
-            }
-            if (x0 > x1) {
-                var x = x0;
-                x0 = x1;
-                x1 = x;
-
-                var y = y0;
-                y0 = y1;
-                y1 = y;
-            }
-            var dx = x1 - x0,
-                dy = Math.abs(y1 - y0),
-                error = 0,
-                de = dy / dx,
-                yStep = -1,
-                y = y0;
-        }
-        if (y0 < y1) {
-            yStep = 1;
-        }
-        for (var x = x0; x < x1; x++) {
-            if (steep) {
-                paint(y, x);
-            } else {
-                paint(x, y);
-            }
-
-            error += de;
-            if (error >= 0.5) {
-                y += yStep;
-                error -= 1.0;
-            }
-        }
-        lastX = mouseX;
-        lastY = mouseY;
+    board.addEventListener('pointermove', function (e) {
+        calculateCoordinates(e);
     });
-    window.addEventListener('mouseup', function () {
+    window.addEventListener('pointerup', function () {
         down = false;
     });
 }
 
-function gridToggle() {
-    grid.addEventListener('mousedown', function () {
-        tiles.forEach(function(e){
-            e.classList.toggle('grid');
-        }); 
-    });
-    pushButtons(grid);
-}
-function eraseAllColor() {
-    eraseAll.addEventListener('click', function () {
-        tiles.forEach(function(e){
-            e.style.removeProperty('background-color');
-        })
-    });
-    pushButtons(eraseAll);
-}
 function sliderChange() {
-    slider.addEventListener('mouseup', function () {
+    slider.addEventListener('pointerup', function () {
         removeTiles();
         let sliderInt = slider.value;
         createBoard(sliderInt);
@@ -333,7 +161,6 @@ function sliderChange() {
     slider.addEventListener('input', function () {
         gridValue.textContent = slider.value + " x " + slider.value;
     });
-
 }
 function removeTiles() {
     while (board.childElementCount != 0) {
@@ -341,29 +168,111 @@ function removeTiles() {
         tiles.pop();
     }
 }
-function paintBlackButton() {
 
-    painterBlack.addEventListener('mousedown', paintBlack);
+function paintBlackButton() {
+    painterBlack.addEventListener('pointerdown', paintBlack);
     pushButtons(painterBlack);
 }
-function paintColorButton() {
 
+function paintColorButton() {
     color.addEventListener('input', paintColor);
     pushButtons(color);
 }
-function eraserButton() {
 
-    eraser.addEventListener('mousedown', eraseColor);
+function eraserButton() {
+    eraser.addEventListener('pointerdown', eraseColor);
     pushButtons(eraser);
+}
+
+function saveColorButton() {
+    saveColor.style.backgroundColor = paintingColor;
+    saveColor.addEventListener('pointerdown', paintColor)
+    pushButtons(saveColor);
+}
+
+function gridToggleButton() {
+    grid.addEventListener('pointerdown', function () {
+        tiles.forEach(function (e) {
+            e.classList.toggle('grid');
+        });
+    });
+    pushButtons(grid);
+}
+
+function eraseAllColorButton() {
+    eraseAll.addEventListener('pointerdown', function () {
+        tiles.forEach(function (e) {
+            e.style.removeProperty('background-color');
+        })
+    });
+    pushButtons(eraseAll);
+}
+
+function calculateCoordinates(e) {
+    if (down) {
+        var mouseX = e.pageX;
+        var mouseY = e.pageY;
+
+        var x0 = mouseX,
+            y0 = mouseY,
+            x1 = lastX,
+            y1 = lastY;
+
+        var steep = (Math.abs(y1 - y0) > Math.abs(x1 - x0));
+        if (steep) {
+            var x = x0;
+            x0 = y0;
+            y0 = x;
+
+            var y = y1;
+            y1 = x1;
+            x1 = y;
+        }
+        if (x0 > x1) {
+            var x = x0;
+            x0 = x1;
+            x1 = x;
+
+            var y = y0;
+            y0 = y1;
+            y1 = y;
+        }
+        var dx = x1 - x0,
+            dy = Math.abs(y1 - y0),
+            error = 0,
+            de = dy / dx,
+            yStep = -1,
+            y = y0;
+    }
+    if (y0 < y1) {
+        yStep = 1;
+    }
+    for (var x = x0; x < x1; x++) {
+        if (steep) {
+            paint(y, x);
+        } else {
+            paint(x, y);
+        }
+
+        error += de;
+        if (error >= 0.5) {
+            y += yStep;
+            error -= 1.0;
+        }
+    }
+    lastX = mouseX;
+    lastY = mouseY;
 }
 
 function start() {
     createBoard(17);
     paintBlackButton();
     paintColorButton();
+    saveColorButton();
     eraserButton();
-    eraseAllColor();
-    gridToggle();
+    eraseAllColorButton();
+    gridToggleButton();
     sliderChange();
 }
+
 start();
